d1b096da9c2b15d409c4f2ab99d7727fae5b8837
idev74 <idevassy77@gmail.com>
2024-04-17 18:34:46 -0700

    completed challenges

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Platform, Button } from 'react-native';
+import { cats, dogs } from './breeds'
+import Item from './Item'
 
 export default function App() {
+  const [search, setSearch] = useState('')
+  const [showDogs, setShowDogs] = useState(true)
+
+  const showAnimal = showDogs ? dogs : cats
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardAvoidContainer}>
+        <StatusBar style="auto" />
+        <View style={styles.container}>
+          <FlatList
+            data={cats}
+            renderItem={({ item, index }) => <Item data={item} index={index} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.inputContainer}
+            onChangeText={text => setSearch(text)}
+            value={search}
+          />
+          <Button
+            title="Search"
+            onPress={() => console.log('search')}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  keyboardAvoidContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: { 
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    width: 200 
   },
 });
